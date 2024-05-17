4dd8f7f2937d9c2a4e1aa21caf81657de988e25d
Sebastian Schalow <sebastian.schalow@volkswagen.de>
2024-05-17 13:34:38 +0200

    Add int_orders__pivoted with jinja

--- dbtFundamentals/models/marts/core/int_orders__pivoted.sql
+++ dbtFundamentals/models/marts/core/int_orders__pivoted.sql
@@ -7,10 +7,15 @@ with payments as (
 pivoted as (
     select
         order_id,
-        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
-        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
-        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
-        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount
+
+        {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
+        {% for pm in payment_methods %}
+            sum(case when payment_method = '{{ pm }}' then amount else 0 end) as {{ pm }}_account
+            {%- if not loop.last -%}
+                ,
+            {% endif %}
+        {% endfor %}
+
     from payments
     where status = 'completed'
     group by 1
