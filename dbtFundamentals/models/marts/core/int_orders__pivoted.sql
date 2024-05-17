9c018513164a2ed6505c28f292bf8919c105a9b4
Sebastian Schalow <sebastian.schalow@volkswagen.de>
2024-05-17 13:37:24 +0200

    Cleanup unneeded newlines created by jinja % without hyphen

--- dbtFundamentals/models/marts/core/int_orders__pivoted.sql
+++ dbtFundamentals/models/marts/core/int_orders__pivoted.sql
@@ -1,3 +1,5 @@
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
 with payments as (
     select * from {{ ref('stg_payments') }}
     left join {{ ref('stg_orders') }}
@@ -8,13 +10,12 @@ pivoted as (
     select
         order_id,
 
-        {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
-        {% for pm in payment_methods %}
+        {%- for pm in payment_methods %}
             sum(case when payment_method = '{{ pm }}' then amount else 0 end) as {{ pm }}_account
             {%- if not loop.last -%}
                 ,
-            {% endif %}
-        {% endfor %}
+            {%- endif -%}
+        {%- endfor %}
 
     from payments
     where status = 'completed'
