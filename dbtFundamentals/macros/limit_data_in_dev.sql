c0c71c1454376f7d022ca332cd016acd9d274d80
Sebastian Schalow <sebastian.schalow@volkswagen.de>
2024-05-24 11:00:14 +0200

    Adjust limit_data_in_dev macro
    so that in dev only entries up to -1 month from '2018-04-09' are returned

--- dbtFundamentals/macros/limit_data_in_dev.sql
+++ dbtFundamentals/macros/limit_data_in_dev.sql
@@ -1,7 +1,7 @@
 {% macro limit_data_in_dev(column_name) %}
 
-{% if target.name != 'dev' %}
-    where {{ column_name }} >= date_add(today(), -INTERVAL 3 DAY)
+{% if target.name == 'dev' %}
+    where {{ column_name }} >= date_add(DATE '2018-04-09', -INTERVAL 1 MONTH)
 {% endif %}
 
 {% endmacro %}
